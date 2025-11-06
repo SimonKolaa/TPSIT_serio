31792369fa59f15e6f593a8dc073622bf710b2c6
SimonKola <simon.kola@studenti.isissgobetti.it>
2025-11-06 12:16:40 +0100

    aggiunto atributo

--- moto.py
+++ moto.py
@@ -1,7 +1,6 @@
 class Moto:
-    def __init__(self, marca, modello, cilindrata):
+    def __init__(self, marca, modello, cilindrata, colore):
         self.marca = marca
         self.modello = modello
         self.cilindrata = cilindrata
-
-    
+        self.colore = colore
