015ee17cfa8a71eaf89962d3a7d540cb8524b14b
Simon <simon.kola@studenti.isissgobetti.it>
2025-11-18 15:10:58 +0100

    correzione errore sintassi

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from Kola_espreg import EspressioneRegolare
 
 def main():
     #Creazione di un'istanza della classe con un'espressione regolare per numeri interi
-    regex_validator = EspressioneRegolare(r'\d+')
+    regex_validatore = EspressioneRegolare(r'\d+')
 
     #Test di validazione con una stringa che corrisponde
     test_string1 = "123"
-    result1 = regex_validator.valida(test_string1)
+    result1 = regex_validatore.valida(test_string1)
     print(f"Test '{test_string1}': {result1}")
+
 
 
 if __name__ == "__main__":
